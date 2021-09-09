82b80b6b40a5a7c4c01ca006aa7770bf812195dd
YoHype <hey@yohype.com>
2021-09-09 12:40:44 +0200

    Tested and implemented IsLeapYear

--- LeapYear.Tests/UnitTest1.cs
+++ LeapYear.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace LeapYear.Tests
@@ -6,9 +7,26 @@ namespace LeapYear.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestIsDivisibleByFour()
         {
+            Assert.True(Program.IsLeapYear(1604));
+            Assert.False(Program.IsLeapYear(1700));
+            Assert.False(Program.IsLeapYear(1800));
+        }
 
+        [Fact]
+        public void TestIsDivisibleByHundred()
+        {
+            Assert.True(Program.IsLeapYear(4000));
+            Assert.False(Program.IsLeapYear(1905));
+        }
+
+        [Fact]
+        public void TestIsDivisibleByFourhoundred()
+        {
+            Assert.True(Program.IsLeapYear(2000));
+            Assert.True(Program.IsLeapYear(1600));
+            Assert.False(Program.IsLeapYear(1800));
         }
     }
 }
--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -2,11 +2,17 @@
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+
+        public static bool IsLeapYear(int year)
+        {
+            return false;
+        }
+
     }
 }
