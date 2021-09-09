b2478e28d626d6fee981bfbced696c23d095e733
YoHype <86954420+YoHype@users.noreply.github.com>
2021-09-09 13:03:33 +0200

    Tested and implemented IsLeapYear (#2)
    * Tested and implemented IsLeapYear

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -2,11 +2,16 @@
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return false;
         }
     }
 }
